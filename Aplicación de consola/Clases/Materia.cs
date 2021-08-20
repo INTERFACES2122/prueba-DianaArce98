8dccc69d391ef166a23d5cc2c17026f5125c4776
PRIDE TOAD <PRIDE TOAD@DESKTOP-7M1BOJ3>
2021-08-20 17:31:01 -0500

    Agregar archivos de proyecto.

---
+++ Aplicación de consola/Clases/Materia.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicación_de_consola.Clases
+{
+    public class Materia
+    {
+        /// <summary>
+        /// Atributo del nombre de la materia.
+        /// </summary>
+        private string nombre;
+        private int no_alumnos;
+        public string Codigo { get; set; }
+
+        //public Materia() { }
+
+        /// <summary>
+        /// Método de acceso al atributo nombre.
+        /// </summary>
+        public string Nombre 
+        {
+            get
+            {
+                return nombre; 
+            }
+            set
+            {
+                nombre = value; 
+            }
+        }
+
+        public int NoAlumnos
+        {
+            get
+            {
+                return no_alumnos; 
+            }
+            set
+            {
+                no_alumnos = value; 
+            }
+        }
+    }
+}
---
+++ Aplicación de consola/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aplicación_de_consola.Clases;
+
+namespace Aplicación_de_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] arreglo = new int[5];    i<lista.Count
+
+            //List<int> lista = new List<int> { 4, 6 };
+            //lista.Add(7);
+
+            //foreach(int i in lista)
+            //{
+            //    Console.WriteLine("valor " + i);
+            //}
+
+            Alumno alumno = new Alumno("284742", "Diana Ivonne", "Arce Ochoa");
+            Alumno alumno2 = new Alumno();
+            Materia materia = new Materia();
+
+            Console.WriteLine(alumno.ObtenerInformacion());
+
+            materia.Nombre = "Interfaces";
+            materia.NoAlumnos = 15;
+            materia.Codigo = "xd"; 
+
+            Console.WriteLine(materia.NoAlumnos);
+            Console.WriteLine(materia.Codigo);
+
+            Console.Read();
+        }
+    }
+}
